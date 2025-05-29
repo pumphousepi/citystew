8ddca499e4592897ade5af763fa62a689c745b70
pumphousepi <pumphousepi@gmail.com>
2025-05-29 17:22:14 -0500

    Your commit message

---
+++ src/app/tailwind.config.js
@@ -0,0 +1,19 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      './src/app/**/*.{js,ts,jsx,tsx}',
+      './src/components/**/*.{js,ts,jsx,tsx}',
+    ],
+    theme: {
+      extend: {},
+    },
+    corePlugins: {
+      fontWeight: true, // ensure font‑semibold, font‑bold, etc. are generated
+    },
+    safelist: [
+      'font-semibold',
+    ],
+    plugins: [],
+  };
+  
