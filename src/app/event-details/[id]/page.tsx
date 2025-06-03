e362c14b20f79eea133ebbf9facbaba943ac6f98
pumphousepi <pumphousepi@gmail.com>
2025-06-03 15:50:58 -0500

    Your commit message

--- src/app/event-details/[id]/page.tsx
+++ src/app/event-details/[id]/page.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import React, { useEffect, useState } from 'react';
 import { notFound } from 'next/navigation';
 
 export const dynamic = 'force-dynamic'; // ensure server-side rendering on each request (optional)
